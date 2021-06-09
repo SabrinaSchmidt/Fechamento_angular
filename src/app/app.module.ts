2db7afa30524a75407efa2381f63b5e2cb6c5207
Sabrina <sabrinaschmidt335@gmail.com>
2021-06-09 09:03:37 -0300

    Commit inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SegundaTelaComponent } from './segunda-tela/segunda-tela.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SegundaTelaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
